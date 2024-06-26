2adc8e2769394de93921786d05adb5c3cbb80cc6
Kah-PretaTech <katilene.santos32@gmail.com>
2024-06-26 00:14:44 -0300

    meu primeiro projeto java

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Boas vindas turma DIO");
+        System.out.println("Uma frase não foi suficiente para alterar commit");
+    }
+}
